3e54e77a63dc130f45a44a0d5242f8a49525227a
Yuqiwu <ywu@stuy.edu>
2018-03-25 17:04:27 -0400

    finished

--- bars.js
+++ bars.js
@@ -1,22 +1,51 @@
-var data = [4, 8, 15, 16, 23, 42];
+var but1 = document.getElementById("2007")
+var but2 = document.getElementById("2016")
+var o = document.getElementById("p");
+
+var spending = [ "Social Secutirty, Unemployment, and Labor", "Medicare and Health", "National Defense", "Net Interest"];
+var year_2007 = [983, 663, 569, 245];
+var year_2016 = [1310, 1000, 541, 215];
+
 var chart = d3.select(".chart");
 var bar = chart.selectAll("div");
-var barUpdate = bar.data(data);
+var barUpdate = bar.data(year_2007);
 var barEnter = barUpdate.enter().append("div");
 
+var display = function(){
+    //instant transition:
+    barEnter.style("width", function(d) {
+	return d *0+ "px"; });
+    barEnter.transition().duration(5000).style("width", function(d) {
+	return d + "px"; });
+    barEnter.text(function(d) { return "$" + d + " billion"; });
+}
 
-/*
-   USAGE: reload index.html before executing each from the console...
+var tog1 = function(){
+    p.innerHTML = "2007 Budget";
+    bar = chart.selectAll("div");
+    barUpdate = bar.data(year_2007);
+    display();
+}
+
+var tog2 = function(){
+    p.innerHTML = "2016 Budget";
+    bar = chart.selectAll("div");
+    barUpdate = bar.data(year_2016);
+    display();
+}
+
+p.innerHTML = "2007 Budget";
+
 //instant transition:
 barEnter.style("width", function(d) {
-  return d * 10 + "px"; });
+    return d *0+ "px"; });
+
 //5s transition:
 barEnter.transition().duration(5000).style("width", function(d) {
-  return d * 10 + "px"; });
-//trans time prop to bar width
-barEnter.transition().duration( function(d){ return d*1000; } )
-  .style("width", function(d) {
-    return d * 10 + "px"; });
- */
+    return d + "px"; });
 
-barEnter.text(function(d) { return d; });
+
+barEnter.text(function(d) { return "$" + d + " billion"; });
+
+but1.addEventListener("click", tog1);
+but2.addEventListener("click", tog2);
